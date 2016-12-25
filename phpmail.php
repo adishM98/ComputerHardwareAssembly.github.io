97ffaf8562a561c604bc0d1b08374927eae87715
Bhargav S <bhargavs2493@gmail.com>
2016-12-25 21:01:45 +0530

    Update phpmail.php

--- phpmail.php
+++ phpmail.php
@@ -29,8 +29,8 @@
 			$mail->SMTPSecure = "tls";
 			$mail->Host = "smtp.gmail.com";
 			$mail->port = 587;
-			$mail->Username = "computer.hardware.online@gmail.com";
-			$mail->Password = "deepeshcha";
+			$mail->Username = "Email";
+			$mail->Password = "Password";
 			//$mail->SMTPDebug = 2;
 
 		
